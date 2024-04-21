c8f36af49100e083e5caba18a94cb49eebf7aa29
nikita <368050@edu.itmo.ru>
2024-04-21 23:35:32 +0300

    fix table notifications & small faker fixes

--- db/migration/V0.13.0_create_table_notifications.sql
+++ db/migration/V0.13.0_create_table_notifications.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS notifications
     id      SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users (id) NOT NULL,
     type    notification_type             NOT NULL,
-    body    JSON                          NOT NULL,
+    body_id INTEGER                       NOT NULL,
     time    TIMESTAMP                     NOT NULL
 );
